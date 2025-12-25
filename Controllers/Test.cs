055332c09fd0ed564149e4c2d25ae44203935929
Kyth Carael <kythcarael04@gmail.com>
2025-12-25 10:56:08 +0800

    Test update

---
+++ Controllers/Test.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentWebsite.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class Test : Controller
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("API is running");
+    }
+}
